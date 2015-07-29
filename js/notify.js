952d197c4d750350d36f0350fc2a15e10dad0181
Jérémy Guilpin <j.guilpin@PC-70GQMW1.fullace.local>
2015-07-29 17:00:42 +0200

    corrections timer

--- js/notify.js
+++ js/notify.js
@@ -1,15 +1,47 @@
 /**
- * Notify 
+ * Notify
 **/
+
+// requestAnimationFrame
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+// Fade out
+function fadeOut(el, callback) {
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+
+  if (typeof(callback) === 'function') callback(el);
+}
+// Fade in
+function fadeIn(el, display, callback) {
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+
+  if (typeof(callback) === 'function') callback(el);
+}
 
 var Notify = {
   setTime: {},
   count: 0,
-  DOM: function(info, message) {
+  DOM: function(info, message, callback) {
     // On vérifie que le container soit présent dans le DOM
     var container = document.getElementById('notify');
     if (!container) {
-      // Si il n'est pas présent on le créé
       // On met à zéro les variables Globales à la création
       Notify.count = 0;
       Notify.setTime = {};
@@ -18,11 +50,12 @@ var Notify = {
       container.setAttribute('id', 'notify');
       container.style.display = 'none';
       // On le met à la fin du body
-      $('body').append(container);
+      document.body.appendChild(container);
     }
     // Création de l'alerte
     var alert = document.createElement('div');
     alert.className = 'notify ' + info;
+    alert.setAttribute('data-time', Notify.count);
     // Suivant le type d'alerte que l'on souhaite on choisit l'icône FontAwesome
     if (info == 'success') var classes = 'fa-check';
     if (info == 'fail') var classes = 'fa-exclamation-triangle';
@@ -31,27 +64,33 @@ var Notify = {
     var icon = document.createElement('i');
     icon.className = 'fa ' + classes;
     // On l'ajoute à l'alerte
-    $(alert).append(icon);
+    alert.appendChild(icon);
     // On créé le container du message
     var spanMessage = document.createElement('span');
     spanMessage.className = 'notify-message';
     spanMessage.innerHTML = message;
     // On ajoute le message dans l'alerte
-    $(alert).append(spanMessage);
+    alert.appendChild(spanMessage);
     // On ajoute une croix pour supprimer l'alerte
     var cross = document.createElement('span');
     cross.className = 'notify-cross';
     cross.innerHTML = 'x';
-    $(cross).on('click', function(e) {
-      $(alert).fadeOut(function() {
-        if (Notify.setTime[Notify.count]) clearTimeout(Notify.setTime[Notify.count]);
-        $(this).remove();
+    cross.addEventListener('click', function(e) {
+      fadeOut(alert, function(el) {
+        if (el.getAttribute('data-time')) {
+          var dataTime = el.getAttribute('data-time');
+          if (Notify.setTime[dataTime]) clearTimeout(Notify.setTime[dataTime]);
+          if (el) el.parentNode.removeChild(el);
+          Notify.checkAlerts();
+          if (callback && typeof(callback) === 'function') callback();
+        }
       });
       e.preventDefault();
     });
 
-    $(alert).append(cross);
-    $(container).append(alert);
+    alert.appendChild(cross);
+    container.appendChild(alert);
+
     // On stock l'alerte que l'on vient de créé et le container
     var obj = {
       'container': container,
@@ -64,32 +103,41 @@ var Notify = {
     // On définit le temps
     time = time ? time : 5000;
     // On récupère l'objet avec l'alerte créé et le container
-    var obj = Notify.DOM(info, message);
+    var obj = Notify.DOM(info, message, callback);
     // On montre le container
-    $(obj.container).show();
+    obj.container.style.display = 'block';
     // On affiche l'alerte
-    $(obj.alert).fadeIn(function() {
+    fadeIn(obj.alert, 'block', function() {
+      var execTime = function() {
+        if (obj.alert.getAttribute('data-time')) {
+          var dataTime = obj.alert.getAttribute('data-time');
+          // Si le timer est fini on le supprime
+          if (Notify.setTime[dataTime]) clearTimeout(Notify.setTime[dataTime]);
+          // Si le timer est fini on cache l'alerte
+          fadeOut(obj.alert, function(el) {
+            // On supprime l'alerte
+            if (el) el.parentNode.removeChild(el);
+            // On vérifie si il y a encore des alertes sinon on supprime le container
+            Notify.checkAlerts();
+            // Si il y a une callback on l'execute
+            if (callback && typeof(callback) === 'function') callback();
+          });
+        }
+      };
       // On enregistre dans l'objet setTime le timer
-      Notify.setTime[Notify.count] = setTimeout(function() {
-        // Si le timer est fini on le supprime
-        if (Notify.setTime[Notify.count]) clearTimeout(Notify.setTime[Notify.count]);
-        // Si le timer est fini on cache l'alerte
-        $(obj.alert).fadeOut(function() {
-          // Quand le fade est fini
-          // On supprime l'alerte
-          $(obj.alert).remove();
-          // On vérifie si il y a encore des alertes sinon on supprime le container
-          Notify.checkAlerts();
-          // Si il y a une callback on l'execute
-          if (callback && typeof(callback) === 'function') callback();
-        });
-      }, time);
-      // On met à jour le compteur
-      Notify.count += 1;
+      if (obj.alert.getAttribute('data-time')) {
+        var dataTime = obj.alert.getAttribute('data-time');
+        Notify.setTime[dataTime] = window.setTimeout(execTime, time);
+      }
     });
+    // On met à jour le compteur
+    Notify.count += 1;
   },
   checkAlerts: function() {
-    if ($('#notify .notify').length == 0) $('#notify').remove();
+    if (document.querySelectorAll('#notify .notify').length == 0) {
+      var notify = document.querySelector('#notify');
+      notify.parentNode.removeChild(notify);
+    }
   },
   success: function(message, callback, time) {
     Notify.alert('success', message, callback, time);
